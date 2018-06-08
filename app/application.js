6332bed8134fec5ed36333163099a11dc2df8f76
Paul Merrill <pdm@pdm.me>
2018-06-08 03:18:07 -0700

    Move the sprite to a non-default location

--- app/application.js
+++ app/application.js
@@ -1,12 +1,17 @@
 const PIXI = require('pixi.js');
 
-const app = new PIXI.Application({
+const Application = PIXI.Application,
+      loader = PIXI.loader,
+      resources = PIXI.loader.resources,
+      Sprite = PIXI.Sprite;
+
+const app = new Application({
     width: 256,
     height: 256,
 });
 document.body.appendChild(app.view);
 
-PIXI.loader
+loader
     .add([
         'images/baum.png',
         'images/cave.png',
@@ -20,8 +25,12 @@ PIXI.loader
     .load(setup);
 
 function setup() {
-    const wizard = new PIXI.Sprite(
-        PIXI.loader.resources['images/wizard.png'].texture
+    const wizard = new Sprite(
+        resources['images/wizard.png'].texture
     );
+
+    wizard.position.set(96, 96);
+    wizard.scale.set(2, 2);
+
     app.stage.addChild(wizard);
 }
