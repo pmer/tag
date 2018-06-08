1e3cbb8e259028f4a66a0fc072a4bdbfd8e74d47
Paul Merrill <pdm@pdm.me>
2018-06-08 02:41:09 -0700

    Show an image on the screen

--- app/application.js
+++ app/application.js
@@ -1,7 +1,28 @@
 const PIXI = require('pixi.js');
 
-const type = PIXI.utils.isWebGLSupported() ? "WebGL" : "canvas";
-PIXI.utils.sayHello(type);
-
-const app = new PIXI.Application({width: 256, height: 256});
+const app = new PIXI.Application({
+    width: 256,
+    height: 256,
+});
 document.body.appendChild(app.view);
+
+PIXI.loader
+    .add([
+        'images/baum.png',
+        'images/cave.png',
+        'images/forest.png',
+        'images/indoors.png',
+        'images/indoors_2.png',
+        'images/objects.png',
+        'images/player.png',
+        'images/wizard.png',
+    ])
+    .load(setup);
+
+function setup() {
+    //This code will run when the loader has finished loading the image
+    const wizard = new PIXI.Sprite(
+        PIXI.loader.resources['images/wizard.png'].texture
+    );
+    app.stage.addChild(wizard);
+}
