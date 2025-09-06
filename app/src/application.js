a314dceeec50501b0bf55c6dbff8b3a919a8f148
Paul Merrill <pdm@pdm.me>
2025-09-05 18:30:20 -0700

    Add Prettier

--- app/src/application.js
+++ app/src/application.js
@@ -1,38 +1,38 @@
-import {Application, Assets, Sprite} from 'pixi.js';
+import { Application, Assets, Sprite } from "pixi.js";
 
 const urls = {
-    baum: new URL('../assets/images/baum.png', import.meta.url).toString(),
-    cave: new URL('../assets/images/cave.png', import.meta.url).toString(),
-    forest: new URL('../assets/images/forest.png', import.meta.url).toString(),
-    indoors: new URL('../assets/images/indoors.png', import.meta.url).toString(),
-    indoors2: new URL('../assets/images/indoors_2.png', import.meta.url).toString(),
-    objects: new URL('../assets/images/objects.png', import.meta.url).toString(),
-    player: new URL('../assets/images/player.png', import.meta.url).toString(),
-    wizard: new URL('../assets/images/wizard.png', import.meta.url).toString(),
+  baum: new URL("../assets/images/baum.png", import.meta.url).href,
+  cave: new URL("../assets/images/cave.png", import.meta.url).href,
+  forest: new URL("../assets/images/forest.png", import.meta.url).href,
+  indoors: new URL("../assets/images/indoors.png", import.meta.url).href,
+  indoors2: new URL("../assets/images/indoors_2.png", import.meta.url).href,
+  objects: new URL("../assets/images/objects.png", import.meta.url).href,
+  player: new URL("../assets/images/player.png", import.meta.url).href,
+  wizard: new URL("../assets/images/wizard.png", import.meta.url).href,
 };
 
 async function main() {
-    const app = new Application();
-    await app.init({
-        width: 256,
-        height: 256,
-    });
-    document.body.appendChild(app.canvas);
+  const app = new Application();
+  await app.init({
+    width: 256,
+    height: 256,
+  });
+  document.body.appendChild(app.canvas);
 
-    const textures = {
-        wizard: await Assets.load(urls.wizard),
-    };
+  const textures = {
+    wizard: await Assets.load(urls.wizard),
+  };
 
-    const wizard = Sprite.from(textures.wizard);
+  const wizard = Sprite.from(textures.wizard);
 
-    wizard.position.set(96, 96);
-    wizard.scale.set(2, 2);
+  wizard.position.set(96, 96);
+  wizard.scale.set(2, 2);
 
-    app.stage.addChild(wizard);
+  app.stage.addChild(wizard);
 
-    app.ticker.add(() => {
-        wizard.rotation += 0.01;
-    });
+  app.ticker.add(() => {
+    wizard.rotation += 0.01;
+  });
 }
 
 main();
--- electron/main.js
+++ electron/main.js
@@ -1,42 +1,39 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow } from "electron";
 
 const isDev =
-    (
-        Number.parseInt(process.env.ELECTRON_IS_DEV, 10) === 1 ||
-        !app.isPackaged
-    ) &&
-    !process.env.ELECTRON_IS_PROD;
+  (Number.parseInt(process.env.ELECTRON_IS_DEV, 10) === 1 || !app.isPackaged) &&
+  !process.env.ELECTRON_IS_PROD;
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        //webPreferences: {
-        //    preload: path.join(__dirname, 'preload.js'),
-        //},
-    });
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    //webPreferences: {
+    //    preload: path.join(__dirname, 'preload.js'),
+    //},
+  });
 
-    if (isDev) {
-        mainWindow.loadURL('http://localhost:1234');
-    } else {
-        mainWindow.loadURL(new URL('../dist/index.html', import.meta.url).href);
-    }
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:1234");
+  } else {
+    mainWindow.loadURL(new URL("../dist/index.html", import.meta.url).href);
+  }
 
-    // mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
-    createWindow();
+  createWindow();
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
